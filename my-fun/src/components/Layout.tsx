9958998e1ac9b2ac13fb2b18569320dc4f1aaddf
Zach Philipp <mrphilipp7@gmail.com>
2023-03-02 20:28:56 +0000

    set up home screen

--- my-fun/src/components/Layout.tsx
+++ my-fun/src/components/Layout.tsx
@@ -6,7 +6,6 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <main className="relative flex h-screen bg items-center">{children}</main>
-      <footer className="flex-1 bg-gray-500">Content</footer>
     </>
   );
 };
